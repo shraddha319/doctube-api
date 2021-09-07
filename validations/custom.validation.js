ce2aef42c50db11656fac1f9ddb8fb8a6cd00907
shraddha319 <shraddha.jain3219@gmail.com>
2021-09-07 20:15:16 +0530

    fix: update regexp for password

--- validations/custom.validation.js
+++ validations/custom.validation.js
@@ -7,12 +7,15 @@ const objectId = (value, helpers) => {
 };
 
 const password = (value, helpers) => {
-  const regexp = /[a-zA-Z]/;
+  const regexp = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+
   if (value.length < 8) {
     return helpers.message('Password must be atleast 8 characters long');
   }
   if (!regexp.test(value)) {
-    return helpers.message('password must contain letters only');
+    return helpers.message(
+      'Password must contain at least one letter, one number and one special character.',
+    );
   }
   return value;
 };
